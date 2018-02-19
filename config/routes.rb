15c92fdda77ed9943cf860c034b07a160e7b8d68
mosei1 <mosei1@outlook.com>
2018-02-18 21:47:11 -0600

    added fields to controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
 
   resources :rides
-  devise_for :users
+  devise_for :users, :controllers => { registrations: 'registrations' }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'users/:id' =>'users#show', as: :view_profile
 end

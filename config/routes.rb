a9be4b5f1cb440290dd95222d92b54d530bd7edb
mosei1 <mosei1@outlook.com>
2018-02-18 21:09:52 -0600

    user view profile created

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  root to: "rides#index"
+
+
   resources :rides
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: "rides#index"
+  get 'users/:id' =>'users#show', as: :view_profile
 end
 
